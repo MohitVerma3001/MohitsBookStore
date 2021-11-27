84744fc012605570cd6a8fdb951934323bf599c7
MohitVerma3001 <W0777694@myscc.ca>
2021-11-27 03:42:47 -0500

    update migration file

--- MohitsBooks.DataAccess/Data/ApplicationDbContext.cs
+++ MohitsBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using MohitsBooks.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
--- MohitsBooks.Models/Category.cs
+++ MohitsBooks.Models/Category.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MohitsBooks.Models
 {
-    class Category
+    public class Category
     {
+        [Key]
+        public int Id { get; set;  }
+        [Display(Name="Category Name")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
     }
 }
